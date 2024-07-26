db831547877f3601ab2ca72b2397fec7ea95dc1c
GPRNomade <guadapadinrojas@gmail.com>
2024-07-26 19:08:58 -0300

    feat: update vaccines relationship and custom forms in admin

--- backend/myvet/forms.py
+++ backend/myvet/forms.py
@@ -1,90 +1,24 @@
 from django import forms
-from .models import Pet, PetType, Product, ProductType, User, UserType, Vaccine, VaccineType
+from django.utils.safestring import mark_safe
+from .models import Pet, Vaccine
+
+class VaccineListWidget(forms.Widget):
+    def render(self, name, value, attrs=None, renderer=None):
+        if value is None or value is "":
+            return mark_safe(f'<p>No vaccines</p>')
+        elif isinstance(value, str):
+            value = value.split(', ')
+        list_items = ''.join([f'<li>{vaccine}</li>' for vaccine in value])
+        return mark_safe(f'<ul style="margin-left: 0; border: 1px solid #fff; border-radius: 5px">{list_items}</ul>')
 
 class PetForm(forms.ModelForm):
-    pet_type_name = forms.CharField(label='Pet Type', required=False)
+    vaccines = forms.CharField(label='Vaccines', required=False, widget=VaccineListWidget)
 
     class Meta:
         model = Pet
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        super(PetForm, self).__init__(*args, **kwargs)
-        if self.instance and hasattr(self.instance, 'type') and self.instance.type:
-            self.fields['pet_type_name'].initial = self.instance.type.name
-
-    def save(self, commit=True):
-        pet = super(PetForm, self).save(commit=False)
-        pet_type_name = self.cleaned_data.get('pet_type_name')
-        if pet_type_name:
-            pet_type, created = PetType.objects.get_or_create(name=pet_type_name)
-            pet.type = pet_type
-        if commit:
-            pet.save()
-        return pet
-
-class ProductForm(forms.ModelForm):
-    product_type_name = forms.CharField(label='Product Type', required=False)
-
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super(ProductForm, self).__init__(*args, **kwargs)
-        if self.instance and hasattr(self.instance, 'type') and self.instance.type:
-            self.fields['product_type_name'].initial = self.instance.type.name
-
-    def save(self, commit=True):
-        product = super(ProductForm, self).save(commit=False)
-        product_type_name = self.cleaned_data.get('product_type_name')
-        if product_type_name:
-            product_type, created = ProductType.objects.get_or_create(name=product_type_name)
-            product.type = product_type
-        if commit:
-            product.save()
-        return product
-    
-class UserForm(forms.ModelForm):
-    user_type_name = forms.CharField(label='User Type', required=False)
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        if self.instance and hasattr(self.instance, 'type') and self.instance.type:
-            self.fields['user_type_name'].initial = self.instance.type.name
-
-    def save(self, commit=True):
-        user = super(UserForm, self).save(commit=False)
-        user_type_name = self.cleaned_data.get('user_type_name')
-        if user_type_name:
-            user_type, created = UserType.objects.get_or_create(name=user_type_name)
-            user.type = user_type
-        if commit:
-            user.save()
-        return user
-    
-# class VaccineForm(forms.ModelForm):
-#     vacc_type_name = forms.CharField(label='Vaccine Name', required=False)
-
-#     class Meta:
-#         model = Vaccine
-#         fields = '__all__'
-
-#     def __init__(self, *args, **kwargs):
-#         super(VaccineForm, self).__init__(*args, **kwargs)
-#         if self.instance and hasattr(self.instance, 'type') and self.instance.type:
-#             self.fields['vacc_type_name'].initial = self.instance.type.name
-
-#     def save(self, commit=True):
-#         vacc = super(VaccineForm, self).save(commit=False)
-#         vacc_type_name = self.cleaned_data.get('vacc_type_name')
-#         if vacc_type_name:
-#             vacc_type, created = VaccineType.objects.get_or_create(name=vacc_type_name)
-#             vacc.type = vacc_type
-#         if commit:
-#             vacc.save()
-#         return vacc
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.vaccines:
+            self.fields['vaccines'].initial = ', '.join(self.instance.vaccines)
--- backend/myvet/models.py
+++ backend/myvet/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class ProductType(models.Model):
     name = models.CharField(max_length=200)
@@ -17,7 +19,7 @@ class UserType(models.Model):
 
 class VaccineType(models.Model):
     name = models.CharField(max_length=200)
-    pet_type = models.ForeignKey(PetType, on_delete=models.CASCADE)
+    pet_type = models.ForeignKey('PetType', on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
@@ -26,8 +28,8 @@ class Product(models.Model):
     description = models.CharField(max_length=200)
     quantity = models.IntegerField()
     price = models.IntegerField(default=100)
-    type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
-    pet_type = models.ForeignKey(PetType, on_delete=models.CASCADE)
+    type = models.ForeignKey('ProductType', on_delete=models.CASCADE)
+    pet_type = models.ForeignKey('PetType', on_delete=models.CASCADE)
     img = models.CharField(default=10) #modificar luego
     pub_date = models.DateTimeField("date published")
     def __str__(self):
@@ -38,26 +40,34 @@ class Pet(models.Model):
     type = models.ForeignKey(PetType, on_delete=models.CASCADE)
     breed = models.CharField(max_length=200, default="")
     age = models.IntegerField(default=100)
-    vaccines = models.JSONField()
+    vaccines = models.JSONField(null=True)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.name
 
 class User(models.Model):
-    type = models.ForeignKey(UserType, on_delete=models.CASCADE)
+    type = models.ForeignKey('UserType', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     dni = models.IntegerField(max_length=8)
-    adress = models.CharField(max_length=200, default="")
+    address = models.CharField(max_length=200, default="")
     email = models.EmailField()   
     password = models.CharField(default=8, max_length=8)
-    pet = models.ForeignKey(Pet, null=True, on_delete=models.CASCADE)
+    pet = models.ForeignKey('Pet', null=True, on_delete=models.CASCADE)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.name
     
 class Vaccine(models.Model):
-    type = models.ForeignKey(VaccineType, on_delete=models.CASCADE)
+    type = models.ForeignKey('VaccineType', on_delete=models.CASCADE)
     app_date = models.DateTimeField("application date")
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    pet = models.ForeignKey('Pet', on_delete=models.CASCADE)
     def __str__(self):
-        return self.type, self.app_date, self.pet
+        return self.type.name
+    
+@receiver(post_save, sender=Vaccine)
+def update_pet_vaccines(sender, instance, **kwargs):
+    pet = instance.pet
+    if pet.vaccines is None or pet.vaccines is "":
+        pet.vaccines = []
+    pet.vaccines.append(instance.type.name)
+    pet.save()
