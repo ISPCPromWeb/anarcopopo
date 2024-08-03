e1382efb7f1b8535b41233b86ab2db1e5ff67782
GPRNomade <guadapadinrojas@gmail.com>
2024-08-03 19:23:59 -0300

    fix: update clients page

--- frontend/app/(with-layout)/dashboard/clients/page.tsx
+++ frontend/app/(with-layout)/dashboard/clients/page.tsx
@@ -1,17 +1,9 @@
-import { UserForm } from "@/components/UserForm";
 import styles from "./page.module.css";
-/* import { sessionId } from "@/utils/auth"; */
-import { redirect } from "next/navigation";
 import { userApi } from "@/api";
 import Link from "next/link";
 
 const Clients = async () => {
   const clients = await userApi.getAll();
-
-  /* if (!sessionId) {
-    redirect("/login");
-    return null;
-  } */
 
   return (
     <div className={styles.clientsList}>
